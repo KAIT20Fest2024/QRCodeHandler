0a7c2f37fd040da02a716569908ae8509f39b419
Sareff <ilbezr@gmail.com>
2024-02-25 17:01:18 +0300

    Updated requirements so you can update everything

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'test_secret_key_22734123'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'postgresql://postgres@localhost:5432/KAIT20FEST'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
