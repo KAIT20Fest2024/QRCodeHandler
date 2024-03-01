992e33727cc61e8e24c4bb1862098a37a900b846
Michael <mixanidze3521@gmail.com>
2024-03-01 11:57:47 +0300

    develop: refactoring

--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,62 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, redirect, url_for, flash, request
-from app import app, db, login_manager
-from app.forms import LoginForm, SignupForm
-
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
-from app.models import User, MasterClass
-
 import qrcode, base64
 
+from app import app, db, login_manager
+from app.forms import LoginForm, SignupForm
+from app.models import User, MasterClass
 
-def admin_required(route):
-    def admin_wrapper(*args, **kwargs):
-        if current_user.login == "ADMIN":
-            return route(*args, **kwargs)
-        else:
-            return redirect(url_for("index"))
-    return admin_wrapper
 
+# --- General ---
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/user/<username>")
+def profile(username: str):
+    user = User.query.filter(User.login == username).first()
+    return render_template("profile.html", user=user) if hasattr(user, 'uid') else redirect(url_for('index'))
+
+@app.route("/scoreboard")
+def scoreboard():
+    users = User.query.order_by(User.score.desc()).filter(User.login != "ADMIN")
+    return render_template("scoreboard.html", users=users)
+
+@app.route("/qrhandler/<mc_id>")
+@login_required
+def qrhandler(mc_id):
+    mc = MasterClass.query.get(mc_id)
+
+    if current_user in mc.users: return redirect(url_for("profile", username=current_user.login))
+    else:
+        current_user.score += mc.score
+        mc.users.append(current_user)
+        db.session.commit()
+    
+    return render_template("qrhandler.html", mc_score=mc.score, user_score=current_user.score)
+
+
+# --- Account management --- 
 @login_manager.user_loader
 def user_loader(uid: int):
     return User.query.get(int(uid))
 
-@app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html")
+@login_manager.unauthorized_handler
+def unathorized():
+    return redirect(url_for("login"))
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        new_user = User(login=form.username.data, first_name=form.fname.data, father_name=form.mname.data, last_name=form.lname.data, school_name=form.school.data, password=form.password.data)
+        new_user = User(login=form.username.data, 
+                        first_name=form.fname.data, 
+                        father_name=form.mname.data, 
+                        last_name=form.lname.data, 
+                        school_name=form.school.data, 
+                        password=form.password.data,
+                        )
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
@@ -41,14 +67,14 @@ def signup():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for("profile", username=current_user.login))
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(login=form.username.data, password=form.password.data).first()
         if user:
             login_user(user, remember=form.remember_me.data)
             return redirect(url_for("profile", username=user.login))
-        else:
-            flash("Wrong credentials")
+        else: flash("Wrong credentials")
     return render_template("login.html", form=form)
 
 @app.route("/logout", methods=["GET"])
@@ -57,64 +83,45 @@ def logout():
     logout_user()
     return redirect(url_for("profile", username=username))
 
-@login_manager.unauthorized_handler
-def unathorized():
-    return redirect(url_for("login"))
 
-@app.route("/user/<username>")
-def profile(username: str):
-    user = User.query.filter(User.login == username).first()
-    return render_template("profile.html", user=user) if hasattr(user, 'uid') else redirect(url_for('index'))
+# --- Admin stuff ---
+def admin_required(route):
+    def admin_wrapper(*args, **kwargs):
+        if current_user.login == "ADMIN": return route(*args, **kwargs)
+        return redirect(url_for("index"))
+    return admin_wrapper
 
 @admin_required
-@app.route("/admin", methods=["GET", "POST"])
+@app.route("/admin", methods=["GET"])
 def admin():
-    masterClasses = MasterClass.query.all()
-    for m in masterClasses:
-        img = qrcode.make(f"http://192.168.202.116:8080/qrhandler/{m.uid}")
-        img.save('app/'+url_for('static', filename='qr/')+str(m.uid)+'.png')
-    return render_template("admin.html", mc = masterClasses)
-
-@admin_required
-@app.route("/admin/edit/<count>", methods=["GET", "POST"])
-def edit(count):
-    mc = MasterClass.query.get(count)
-    if mc:
-        if request.method == 'GET':
-            return render_template("edit.html", mc=mc)
-        mc.name = request.form.get('mc_name')
-        mc.context = request.form.get('context')
-        mc.score = request.form.get('score')
-        db.session.commit()
-    return redirect(url_for('admin'))
+    mcs = MasterClass.query.all()
+    return render_template("admin.html", mc=mcs)
 
 @admin_required
 @app.route("/admin/create", methods=["GET", "POST"])
 def create():
     if request.method == "POST":
-        mc = MasterClass(name=request.form.get('name'),context=request.form.get('context'),score=request.form.get('score'))
+        mc = MasterClass(name=request.form.get('name'), context=request.form.get('context'), score=request.form.get('score'))
         db.session.add(mc)
         db.session.commit()
+
+        mc_filename = f'{mc.uid}{mc.name[0]}{mc.score}'
+        img = qrcode.make(f"localhost/qrhandler/{m_filename}")
+        img.save(f"app/{url_for('static', filename='qr/')}{m_filename}.png")
+
         return redirect(url_for('admin'))
 
     return render_template("create.html")
 
-@app.route("/scoreboard")
-def scoreboard():
-    users = User.query.order_by(User.score.desc()).filter(User.login != "ADMIN")
-    return render_template("scoreboard.html", users=users)
-
-@app.route("/qrhandler/<mcid>")
-@login_required
-def qrhandler(mcid):
-    mc = MasterClass.query.get(mcid)
-
-    if current_user in mc.users:
-        return redirect(url_for("profile", username=current_user.login))
-    else:
-        current_user.score += mc.score
-        mc.users.append(current_user)
+@admin_required
+@app.route("/admin/edit/<mc_id>", methods=["GET", "POST"])
+def edit(mc_id):
+    mc = MasterClass.query.get(mc_id)
+    if mc:
+        if request.method == 'GET': return render_template("edit.html", mc=mc)
+        mc.name = request.form.get('mc_name')
+        mc.context = request.form.get('context')
+        mc.score = request.form.get('score')
         db.session.commit()
-    
-    return render_template("qrhandler.html", mc_score=mc.score, user_score=current_user.score)
+    return redirect(url_for('admin'))
 
